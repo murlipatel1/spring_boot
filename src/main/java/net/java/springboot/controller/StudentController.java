c7841ff2c9e90c7767bed55c95c3a81915ec1067
Murli Patel <patelmurli15@gmail.com>
2023-07-12 17:47:09 +0530

    all rest api example

--- src/main/java/net/java/springboot/controller/StudentController.java
+++ src/main/java/net/java/springboot/controller/StudentController.java
@@ -1,14 +1,86 @@
 package net.java.springboot.controller;
 
+import com.fasterxml.jackson.databind.util.AccessPattern;
 import net.java.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
+    // http://localhost:8081/students/student
     @GetMapping("student")
-    public Student getStudent(){
-        Student std = new Student(2,"Murli","Patel");
-        return std;
+    public ResponseEntity<Student> getStudent(){
+        Student student = new Student(
+                1,
+                "murli",
+                "patel"
+        );
+        // return new ResponseEntity<>(student, HttpStatus.OK);
+        return ResponseEntity.ok()
+                .header("custom-header", "murli")
+                .body(student);
     }
+
+    // http://localhost:8081/students
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "murli", "patel"));
+        students.add(new Student(2, "dev", "patel"));
+
+        return ResponseEntity.ok(students);
+    }
+
+    // Spring BOOT REST API with Path Variable
+    // {id} - URI template variable
+    // http://localhost:8081/students/1/murli/patel
+    @GetMapping("{id}/{first-name}/{last-name}")
+    public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId,
+                                                       @PathVariable("first-name") String firstName,
+                                                       @PathVariable("last-name") String lastName){
+        Student student = new Student(studentId, firstName, lastName);
+        return ResponseEntity.ok(student);
+    }
+
+    // Spring boot REST API with Request Param
+    //  http://localhost:8081/students/query?id=1&firstName=murli&lastName=patel
+    @GetMapping("query")
+    public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
+                                                          @RequestParam String firstName,
+                                                          @RequestParam String lastName){
+        Student student = new Student(id, firstName, lastName);
+        return ResponseEntity.ok(student);
+    }
+
+    // Spring boot REST API that handles HTTP POST Request - creating new resource
+    // @PostMapping and @RequestBody
+    @PostMapping("create")
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstname());
+        System.out.println(student.getLastname());
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    // Spring boot REST API that handles HTTP PUT Request - updating existing resource
+    @PutMapping("{id}/update")
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId){
+        System.out.println(student.getFirstname());
+        System.out.println(student.getLastname());
+        return ResponseEntity.ok(student);
+    }
+
+    // Spring boot REST API that handles HTTP DELETE Request - deleting the existing resource
+    @DeleteMapping("{id}/delete")
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId){
+        System.out.println(studentId);
+        return ResponseEntity.ok("Student deleted successfully!");
+    }
+
 }
